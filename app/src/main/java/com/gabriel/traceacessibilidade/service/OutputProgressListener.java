3cff8555f54fb0cc6f9f6f8894739e9d320166a1
GabrielJacquier <gabriel.jacquier@tegra.me>
2016-11-06 22:39:44 -0200

    Add class listener to user message

---
+++ app/src/main/java/com/gabriel/traceacessibilidade/service/OutputProgressListener.java
@@ -0,0 +1,41 @@
+package com.gabriel.traceacessibilidade.service;
+
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+
+import com.gabriel.traceacessibilidade.view.MainActivity;
+
+/**
+ * Created by gabriel on 06/11/16.
+ */
+
+public class OutputProgressListener extends UtteranceProgressListener {
+
+    private InputVoiceMessageService inputVoiceMessageService;
+    private MainActivity activityMain;
+
+    public OutputProgressListener(MainActivity activity, InputVoiceMessageService inputVoiceMessageService) {
+        this.activityMain = activity;
+        this.inputVoiceMessageService = inputVoiceMessageService;
+    }
+
+    @Override
+    public void onStart(String utteranceId) {
+
+    }
+
+    @Override
+    public void onDone(String utteranceId) {
+        activityMain.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                inputVoiceMessageService.listeningUser();
+            }
+        });
+    }
+
+    @Override
+    public void onError(String utteranceId) {
+
+    }
+}
